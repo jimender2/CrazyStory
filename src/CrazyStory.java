f4211f128f8908bbf31b5ec51cccec3cd2efe862
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-02-07 10:16:57 -0500

    Update CrazyStory.java

--- src/CrazyStory.java
+++ src/CrazyStory.java
@@ -23,30 +23,41 @@ public class CrazyStory {
 			System.out.print("Enter an adjective: ");
 			adjective = keyboard.nextLine();
 
+			//Get State
 			System.out.print("Enter your State: ");
 			state = keyboard.nextLine();
 
+			//Get Age and clear newline
 			System.out.print("Enter your age: ");
 			age = keyboard.nextInt();
 			keyboard.nextLine();
+			
+			//Get College
 			System.out.print("Enter your College: ");
 			college = keyboard.nextLine();
 
+			//Get Profession
 			System.out.print("Enter your Profession: ");
 			profession = keyboard.nextLine();
 
+			//Get Pay and clear newline
 			System.out.print("Enter your Pay per hour: ");
 			pay = keyboard.nextDouble();
 			keyboard.nextLine();
-			System.out.print("Enter your Animal: ");
+		
+			//Get Animal
+			System.out.print("Enter an Animal Name: ");
 			animal = keyboard.nextLine();
 
+			//Get Pet Name
 			System.out.print("Enter your Pet Name: ");
 			petName = keyboard.nextLine();
 
+			//Get Verb
 			System.out.print("Enter a verb: ");
 			verb = keyboard.nextLine();
 			
+			//Print Story
 			System.out.println("Once upon a time someone named " + name + " lived in " + city +
 					" in the " + adjective + " state of " + state + ".\nWhen " + age + " years old, "
 					+ name +" went to school at " + college + ".\n" + name + " graduated and went to work as a "
